c600607d7089ffc2a9d4334bbb59e7f636d94de6
Ankitkm95 <ankitmishra517@gmail.com>
2022-11-09 23:35:34 +0530

    navbar

---
+++ components/navbar.js
@@ -0,0 +1,248 @@
+let navbar = () => {
+    return ` <div><img src="hero.png" alt="">
+    </div>
+    <div class="input">
+        <input type="text" placeholder="What service are you looking for today?">
+        <button><i class="fa-solid fa-magnifying-glass"></i></i></button>
+    </div>
+    
+    <a href="">Fleverr Business</a>
+    <a href="">Explore</a>
+    <a href=""><i class="fa-solid fa-globe"></i> English</a>
+    <a href=""><i class="fa-solid fa-indian-rupee-sign"></i> INR</a>
+    <a href="">Become a Seller</a>
+    <a href="">Sign in</a>
+    <button>Join</button>`
+}
+
+let navbar2 = () => {
+    return `<div id="header2">
+    <div class="dropDown">
+        <a href="">Graphic & Design</a>
+        <div class="dropDownCnt">
+            <div class="hover1">
+                <div>
+                    <h3>Logo & Brand Identity</h3>
+                    <a href="">Logo Design</a>
+                    <a href="">Brand Styles Guides</a>
+                    <a href="">Fonts & Typography</a>
+                    <a href="">Business Cards & Stationery</a>
+                    <h3>Gaming</h3>
+                    <a href="">Game Art</a>
+                    <a href="">Graphics for Streamers</a>
+                    <a href="">Twitch Store</a>
+                    <h3>Art & Illustration</h3>
+                    <a href="">Illustration</a>
+                    <a href="">NFT Art</a>
+                    <a href="">Pattern Design</a>
+                    <a href="">Portraits & Caricatures</a>
+                    <a href="">Cartoons & Comics</a>
+                </div>
+                <div>
+                    <h3>Logo & Brand Identity</h3>
+                    <a href="">Logo Design</a>
+                    <a href="">Brand Styles Guides</a>
+                    <a href="">Fonts & Typography</a>
+                    <a href="">Business Cards & Stationery</a>
+                    <h3>Gaming</h3>
+                    <a href="">Game Art</a>
+                    <a href="">Graphics for Streamers</a>
+                    <a href="">Twitch Store</a>
+                    <h3>Art & Illustration</h3>
+                    <a href="">Illustration</a>
+                    <a href="">NFT Art</a>
+                    <a href="">Pattern Design</a>
+                    <a href="">Portraits & Caricatures</a>
+                    <a href="">Cartoons & Comics</a>
+                </div>
+                <div>
+                    <h3>Logo & Brand Identity</h3>
+                    <a href="">Logo Design</a>
+                    <a href="">Brand Styles Guides</a>
+                    <a href="">Fonts & Typography</a>
+                    <a href="">Business Cards & Stationery</a>
+                    <h3>Gaming</h3>
+                    <a href="">Game Art</a>
+                    <a href="">Graphics for Streamers</a>
+                    <a href="">Twitch Store</a>
+                    <h3>Art & Illustration</h3>
+                    <a href="">Illustration</a>
+                    <a href="">NFT Art</a>
+                    <a href="">Pattern Design</a>
+                    <a href="">Portraits & Caricatures</a>
+                    <a href="">Cartoons & Comics</a>
+                </div>
+                <div>
+                    <h3>Logo & Brand Identity</h3>
+                    <a href="">Logo Design</a>
+                    <a href="">Brand Styles Guides</a>
+                    <a href="">Fonts & Typography</a>
+                    <a href="">Business Cards & Stationery</a>
+                    <h3>Gaming</h3>
+                    <a href="">Game Art</a>
+                    <a href="">Graphics for Streamers</a>
+                    <a href="">Twitch Store</a>
+                    <h3>Art & Illustration</h3>
+                    <a href="">Illustration</a>
+                    <a href="">NFT Art</a>
+                    <a href="">Pattern Design</a>
+                    <a href="">Portraits & Caricatures</a>
+                    <a href="">Cartoons & Comics</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="dropDown">
+        <a href="">Digital Marketing</a>
+        <div class="dropDownCnt">
+            <div class="hover2">
+                <div>
+                    <a href="">Social Media Marketing</a>
+                    <a href="">Social Media Advertising</a>
+                    <a href="">Search Engine Optimization (SEO)</a>
+                    <a href="">Local SEO</a>
+                    <a href="">Marketing Strategy</a>
+                    <a href="">Public Relations</a>
+                    <a href="">Guest Posting</a>
+                    <a href="">Video Marketing</a>
+                    <a href="">Email Marketing</a>
+                    <a href="">Web Analytics</a>
+                    <a href="">Text Message Marketing</a>
+                    <a href="">Crowdfunding</a>
+                </div>
+                <div>
+                    <a href="">Marketing Advice</a>
+                    <a href="">Search Engine Marketing (SEM)</a>
+                    <a href="">Display Advertising</a>
+                    <a href="">E-Commerce Marketing</a>
+                    <a href="">Influencer Marketing</a>
+                    <a href="">Community Management</a>
+                    <a href="">Mobile App Marketing</a>
+                    <a href="">Music Promotion</a>
+                    <a href="">Book & eBook Marketing</a>
+                    <a href="">Podcast Marketing</a>
+                    <a href="">Affiliate Marketing</a>
+                    <a href="">Other</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="dropDown">
+        <a href="" class="dropDownBtn">Writing & Translation</a>
+        <div class="dropDownCnt">
+            <div class="hover1">
+                <div>
+                    <h3>Logo & Brand Identity</h3>
+                    <a href="">Logo Design</a>
+                    <a href="">Brand Styles Guides</a>
+                    <a href="">Fonts & Typography</a>
+                    <a href="">Business Cards & Stationery</a>
+                    <h3>Gaming</h3>
+                    <a href="">Game Art</a>
+                    <a href="">Graphics for Streamers</a>
+                    <a href="">Twitch Store</a>
+                    <h3>Art & Illustration</h3>
+                    <a href="">Illustration</a>
+                    <a href="">NFT Art</a>
+                    <a href="">Pattern Design</a>
+                    <a href="">Portraits & Caricatures</a>
+                    <a href="">Cartoons & Comics</a>
+                </div>
+                <div>
+                    <h3>Logo & Brand Identity</h3>
+                    <a href="">Logo Design</a>
+                    <a href="">Brand Styles Guides</a>
+                    <a href="">Fonts & Typography</a>
+                    <a href="">Business Cards & Stationery</a>
+                    <h3>Gaming</h3>
+                    <a href="">Game Art</a>
+                    <a href="">Graphics for Streamers</a>
+                    <a href="">Twitch Store</a>
+                    <h3>Art & Illustration</h3>
+                    <a href="">Illustration</a>
+                    <a href="">NFT Art</a>
+                    <a href="">Pattern Design</a>
+                    <a href="">Portraits & Caricatures</a>
+                    <a href="">Cartoons & Comics</a>
+                </div>
+                <div>
+                    <h3>Logo & Brand Identity</h3>
+                    <a href="">Logo Design</a>
+                    <a href="">Brand Styles Guides</a>
+                    <a href="">Fonts & Typography</a>
+                    <a href="">Business Cards & Stationery</a>
+                    <h3>Gaming</h3>
+                    <a href="">Game Art</a>
+                    <a href="">Graphics for Streamers</a>
+                    <a href="">Twitch Store</a>
+                    <h3>Art & Illustration</h3>
+                    <a href="">Illustration</a>
+                    <a href="">NFT Art</a>
+                    <a href="">Pattern Design</a>
+                    <a href="">Portraits & Caricatures</a>
+                    <a href="">Cartoons & Comics</a>
+                </div>
+                <div>
+                    <h3>Logo & Brand Identity</h3>
+                    <a href="">Logo Design</a>
+                    <a href="">Brand Styles Guides</a>
+                    <a href="">Fonts & Typography</a>
+                    <a href="">Business Cards & Stationery</a>
+                    <h3>Gaming</h3>
+                    <a href="">Game Art</a>
+                    <a href="">Graphics for Streamers</a>
+                    <a href="">Twitch Store</a>
+                    <h3>Art & Illustration</h3>
+                    <a href="">Illustration</a>
+                    <a href="">NFT Art</a>
+                    <a href="">Pattern Design</a>
+                    <a href="">Portraits & Caricatures</a>
+                    <a href="">Cartoons & Comics</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="dropDown">
+        <a href="">Video & Animation</a>
+        <div class="dropDownCnt">
+            <div class="hover2">
+                <div>
+                    <a href="">Social Media Marketing</a>
+                    <a href="">Social Media Advertising</a>
+                    <a href="">Search Engine Optimization (SEO)</a>
+                    <a href="">Local SEO</a>
+                    <a href="">Marketing Strategy</a>
+                    <a href="">Public Relations</a>
+                    <a href="">Guest Posting</a>
+                    <a href="">Video Marketing</a>
+                    <a href="">Email Marketing</a>
+                    <a href="">Web Analytics</a>
+                    <a href="">Text Message Marketing</a>
+                    <a href="">Crowdfunding</a>
+                </div>
+                <div>
+                    <a href="">Marketing Advice</a>
+                    <a href="">Search Engine Marketing (SEM)</a>
+                    <a href="">Display Advertising</a>
+                    <a href="">E-Commerce Marketing</a>
+                    <a href="">Influencer Marketing</a>
+                    <a href="">Community Management</a>
+                    <a href="">Mobile App Marketing</a>
+                    <a href="">Music Promotion</a>
+                    <a href="">Book & eBook Marketing</a>
+                    <a href="">Podcast Marketing</a>
+                    <a href="">Affiliate Marketing</a>
+                    <a href="">Other</a>
+                </div>
+            </div>    
+        </div>
+    </div>
+    <a href="">Music & Audio</a>
+    <a href="">Programming & Tech</a>
+    <a href="">Business</a>
+    <a href="">Lifestyle</a>
+    <a href="">Trading</a>
+</div>`
+}
+
+export { navbar,navbar2 };
---
+++ index.js
@@ -0,0 +1,55 @@
+import { navbar,navbar2 } from './components/navbar.js';
+
+let nav1 = document.getElementById("header1");
+nav1.innerHTML = navbar();
+let nav2 = document.getElementById("nav");
+nav2.innerHTML = navbar2();
+
+
+
+
+window.addEventListener("scroll", function(){
+    if(window.pageYOffset < 96.5){
+       //document.querySelector("#header2").style.visibility="hidden";
+       document.querySelector("#nav").style.display="none"
+        document.querySelector("#header1").style.backgroundColor="#ac3f5b";
+        document.querySelector(".input").style.visibility="hidden";
+        
+    }else{
+        //document.querySelector("#header2").style.visibility="visible";
+        document.querySelector("#nav").style.display="block"
+       //
+       //document.querySelector("#nav").style.position="relative"
+       document.querySelector("#nav").style.position="sticky"
+       document.querySelector("#nav").style.top = "93px"
+        document.querySelector("#header1").style.backgroundColor="white";
+        document.querySelector(".input").style.visibility="visible";
+    }
+    
+});
+
+
+//let carousel_div = document.querySelector("#carousel");
+
+// let carousel = () => {
+//     //	https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/4252/1364252-h-e470328888ed
+//     //	https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/5361/1395361-h-a9af817e06df
+//     //https://fiverr-res.cloudinary.com/q_auto,f_auto,w_1400,dpr_1.0/v1/attachments/generic_asset/asset/50218c41d277f7d85feeaf3efb4549bd-1599072608122/bg-signup-1400-x1.png
+
+//     let imgArr = [`https://fiverr-res.cloudinary.com/q_auto,f_auto,w_1400,dpr_1.0/v1/attachments/generic_asset/asset/50218c41d277f7d85feeaf3efb4549bd-1599072608122/bg-signup-1400-x1.png`,`https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/5361/1395361-h-a9af817e06df`,`https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/4252/1364252-h-e470328888ed`];
+
+//     let imgElement = document.createElement('img');
+//     imgElement.src = imgArr[0];
+//     carousel_div.append(imgElement);
+
+//     let i = 1;
+//     setInterval(() => {
+//         if(i==imgArr.length){
+//             i=0;
+//         }
+//         imgElement.src = imgArr[i];
+//         carousel_div.append(imgElement);
+//         i++;
+//     },5000)
+// }
+// carousel()
