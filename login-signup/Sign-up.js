6e5450b9fee716c14f4c9878f7591e66b1d0758c
Ritikve03 <ritik.rv03@gmail.com>
2022-11-11 11:46:36 +0530

    login completed

--- login-signup/Sign-up.js
+++ login-signup/Sign-up.js
@@ -31,7 +31,7 @@ const addDetails = async (event)=>{
         password,
     };
 
-    let response = await fetch(`http://localhost:3000/user`,{
+    let response = await fetch(`http://localhost:8080/user`,{
 
 
         method:"POST",
@@ -44,4 +44,60 @@ const addDetails = async (event)=>{
 
     let data = await response.json()
     console.log(data);
+}
+
+let login_form = document.getElementById('login_form');
+login_form.addEventListener('submit',function(){
+    getData(event)
+})
+
+
+let admin =[
+    {
+        "email": "tushit@gmail.com",
+        "password": "masai@1234"
+    },
+    {
+        "email": "anik@gmail.com",
+        "password": "masai@1234"
+    },
+    {
+        "email": "faisal@gmail.com",
+        "password": "masai@1234"
+    },
+    {
+        "email": "ritik@gmail.com",
+        "password": "masai@1234"
+    }
+]
+const getData =async (event)=>{
+
+    event.preventDefault();
+    let email = login_form.login_email.value;
+
+    let password = login_form.login_password.value;
+
+    let user = false;
+    data.filter(function(e){
+        if(e.email == email && e.password==password){
+            user = true
+        }
+    })
+    let res = await fetch(' http://localhost:8080/user')
+
+    let details = await res.json();
+    console.log(details)
+    
+    if(password = "masai@1234"){
+        admin.filter((e)=>{
+            if(e.email === email){
+                console.log("OK");
+                // window.location.href=""
+            }
+        })
+    }else if(user == true) {
+        // window.location.href=""
+    }else{
+        alert("invalid user")
+    }
 }
