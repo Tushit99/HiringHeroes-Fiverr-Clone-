03c6e545784e011a11ed3798120ca3708b57aede
Ankitkm95 <ankitmishra517@gmail.com>
2022-11-11 20:40:31 +0530

    completed

---
+++ afterlogin.js
@@ -0,0 +1,6 @@
+import { footer,footer2 } from './components/footer.js';
+
+let foot = document.querySelector("#footer");
+foot.innerHTML = footer();
+let foot2 = document.querySelector("#last2");
+foot2.innerHTML = footer2();
